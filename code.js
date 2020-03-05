4413e0ceb5702cf7ef18066047de0db758575b3a
joooons <joon.s.chang@gmail.com>
2020-03-04 20:46:46 -0800

    removed dependence on id. started using class.

--- code.js
+++ code.js
@@ -6,79 +6,85 @@
 
 
 
+// ------------------------------------------------------ //
 // -------------------- DECLARATIONS -------------------- //
+// ------------------------------------------------------ //
 
 
     var pointer = {
-        max : 0,
-        num : 0,
-        id  : "does it matter what I set this as? Nope."
+        r   : 0,
+        c   : 0,
+        l   : 0
         };
 
 
     var init = {
-        line    : { x1:0,   y1:0,   x2:100,     y2:100,                 lw:2,   lc:'black', ca:'round'  },
-        rect    : { x:0,    y:0,    w:100,      h:100,  co:'yellow',    lw:2,   lc:'black', ra:5        },
-        circle  : { x:0,    y:0,    r:50,               co:'red',       lw:4,   lc:'black'              }
+        line    : { x1:0,   y1:0,   x2:100,     y2:100,               lw:2,   lc:'#fad', ca:'round'  },
+        rect    : { x:0,    y:0,    w:100,      h:100,  co:'#daf',    lw:2,   lc:'#fda', ra:5        },
+        circle  : { x:0,    y:0,    r:50,               co:'#adf',    lw:4,   lc:'#afd'              }
         }
     
+    var color = {
+        shade : '#DAF',
+        blank : '#DEF'
+    }
 
 
-    var code = $('#codeSpace');
+
+    var code =  $('#codeSpace');
     var logic = $('#logicSpace');
-    var art = $('#artSpace');
+    var art =   $('#artSpace');
 
 
 
 
 
 
-// -------------------- FUNCTIONS -------------------- //
+
+
+
+// ------------------------------------------------------ //
+// --------------------- FUNCTIONS ---------------------- //
+// ------------------------------------------------------ //
+
+
+
+
+
+
+function sel(row, col) { return $(`#logicSpace div:nth-child(${row}) input:nth-child(${col})`).val(); }
+    // QUICK way to reference the element through row and col.
+    // Do I use this anywhere else beside in updateSVG()?
+
+
+
 
 function Initiate() {
     // This function loads a new line, but the SVG type has not been identified yet.
     // The user will have to specify whether this element is a line, rect, or circle.
 
+    logic.append(`<div></div>`);
+
     function fl(str) { return `<div class='filler'>${str}</div>`; };
+    let max = $('#logicSpace > div').length;
+    let elem = $(`#logicSpace div:last-child`);
 
-    logic.append(`<div id="elem${pointer.max}"></div>`);
+    elem.append(`${fl('ID')}<input value='${max}' type='number' disabled/>`);
+    
+    let a = "<option value='none'>none</option><option value='line'>line</option><option value='rect'>rect</option><option value='circle'>circle</option>";
+    elem.append(`${fl('type')}<select value='none' class='tp' >${a}</select>`);
+    
 
-
-    $(`#elem${pointer.max}`).append(`${fl('ID')}<input value='${pointer.max}' type='number' id='id${pointer.max}' disabled/>`);
-    // $(`#elem${pointer.max}`).append(`${fl('type')}<input value='what' type='text' id='tp${pointer.max}' />`);
-
-    let a = "<option value='none'>none</option>";
-    a += "<option value='line'>line</option>";
-    a += "<option value='rect'>rect</option>";
-    a += "<option value='circle'>circle</option>";
-    $(`#elem${pointer.max}`).append(`${fl('type')}<select value='none' id='tp${pointer.max}' >${a}</select>`);   
-
-
-    $(`#tp${pointer.max}`).on("focus", function() { $(this).css("background-color", "#DEF"); });
-
-
-    $(`#tp${pointer.max}`).on("change", function() {     
-        if ( /^line$|^rect$|^circle$/.test( $(this).val() ) ) {
-            
-            $(this).attr("disabled", true);
-            Populate( $(this).val() );
-            
-        }
+    $('.tp').on("focus",    function() { $(this).css("background-color", color.shade); });
+    $('.tp').on("focusout", function() { $(this).css("background-color", color.blank); });
+    $('.tp').on("change",   function() {     
+        $(this).attr("disabled", true);
+        Populate( $(this).val() );
     });
+        // CHANGE
 
 
-    $(`#tp${pointer.max}`).on("click", function() {
-        // This is for the drop menu.
-        // Use this later!!!
-
-        let a = $(this).parent().attr("id");
-        // alert(a);
-        // $(`#${a}`).append(`<div position> why </div>  `);
-
-    });
-
-
-}
+}   // END of Initiate()
 
 
 
@@ -93,95 +99,86 @@ function Populate(svgType) {
     // This can only ADD, never subtract.
 
     function fl(str) { return `<div class='filler'>${str}</div>`; };
+    let max = $('#logicSpace > div').length;
+    
+    let elem = `#logicSpace div:last-child`;
+
 
     switch (svgType) {
         case "line":
-            $(`#elem${pointer.max}`).append(`${fl('x1')}<input value='${init.line.x1}' type='number' id="x1${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('y1')}<input value='${init.line.y1}' type='number' id="y1${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('x2')}<input value='${init.line.x2}' type='number' id="x2${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('y2')}<input value='${init.line.y2}' type='number' id="y2${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('  ')}<input value='' id="du${pointer.max}" class="dummy" disabled/>`);
-            $(`#elem${pointer.max}`).append(`${fl('lw')}<input value='${init.line.lw}' type='number' id="lw${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('lc')}<input value='${init.line.lc}' id="lc${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('ca')}<input value='${init.line.ca}' id="ca${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append("<button>+</button>");
+            $(elem).append(`${fl('x1')}<input value='${init.line.x1}' type='number' id="x1${max}" class='x' />`);
+            $(elem).append(`${fl('y1')}<input value='${init.line.y1}' type='number' id="y1${max}" class='y' />`);
+            $(elem).append(`${fl('x2')}<input value='${init.line.x2}' type='number' id="x2${max}" class='x' />`);
+            $(elem).append(`${fl('y2')}<input value='${init.line.y2}' type='number' id="y2${max}" class='y' />`);
+            $(elem).append(`${fl('  ')}<input value='' id="du${max}" class="dummy" disabled/>`);
+            $(elem).append(`${fl('lw')}<input value='${init.line.lw}' type='number' id="lw${max}" />`);
+            $(elem).append(`${fl('lc')}<input value='${init.line.lc}' id="lc${max}" class='co' />`);
+            $(elem).append(`${fl('ca')}<input value='${init.line.ca}' id="ca${max}" />`);
+            $(elem).append("<button>+</button>");
             updateSVG();
             break;
         case "rect":
-            $(`#elem${pointer.max}`).append(`${fl('x ')}<input value='${init.rect.x}' type='number' id="x-${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('y ')}<input value='${init.rect.y}' type='number' id="y-${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('w ')}<input value='${init.rect.w}' type='number' id="w-${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('h ')}<input value='${init.rect.h}' type='number' id="h-${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('co')}<input value='${init.rect.co}' id="co${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('lw')}<input value='${init.rect.lw}' type='number' id="lw${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('lc')}<input value='${init.rect.lc}' id="lc${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('ra')}<input value='${init.rect.ra}' type='number' id="ra${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append("<button>+</button>");
+            $(elem).append(`${fl('x ')}<input value='${init.rect.x}' type='number' id="x-${max}" class='x' />`);
+            $(elem).append(`${fl('y ')}<input value='${init.rect.y}' type='number' id="y-${max}" class='y' />`);
+            $(elem).append(`${fl('w ')}<input value='${init.rect.w}' type='number' id="w-${max}" class='w' />`);
+            $(elem).append(`${fl('h ')}<input value='${init.rect.h}' type='number' id="h-${max}" class='h' />`);
+            $(elem).append(`${fl('co')}<input value='${init.rect.co}' id="co${max}" class='co' />`);
+            $(elem).append(`${fl('lw')}<input value='${init.rect.lw}' type='number' id="lw${max}" />`);
+            $(elem).append(`${fl('lc')}<input value='${init.rect.lc}' id="lc${max}" class='co' />`);
+            $(elem).append(`${fl('ra')}<input value='${init.rect.ra}' type='number' id="ra${max}" />`);
+            $(elem).append("<button>+</button>");
             updateSVG();
             break;
         case "circle":
-            $(`#elem${pointer.max}`).append(`${fl('x ')}<input value='${init.circle.x}' type='number' id="x-${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('y ')}<input value='${init.circle.y}' type='number' id="y-${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('r ')}<input value='${init.circle.r}' type='number' id="r-${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('  ')}<input value='' id="du${pointer.max}" class='dummy' disabled/>`);
-            $(`#elem${pointer.max}`).append(`${fl('co')}<input value='${init.circle.co}' id="co${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('lw')}<input value='${init.circle.lw}' type='number' id="lw${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('lc')}<input value='${init.circle.lc}' id="lc${pointer.max}" />`);
-            $(`#elem${pointer.max}`).append(`${fl('  ')}<input value='' id="du-${pointer.max}" class='dummy' disabled/>`);
-            $(`#elem${pointer.max}`).append("<button>+</button>");
+            $(elem).append(`${fl('x ')}<input value='${init.circle.x}' type='number' id="x-${max}" class='x' />`);
+            $(elem).append(`${fl('y ')}<input value='${init.circle.y}' type='number' id="y-${max}" class='y' />`);
+            $(elem).append(`${fl('r ')}<input value='${init.circle.r}' type='number' id="r-${max}" class='r' />`);
+            $(elem).append(`${fl('  ')}<input value='' id="du${max}" class='dummy' disabled/>`);
+            $(elem).append(`${fl('co')}<input value='${init.circle.co}' id="co${max}" class='co' />`);
+            $(elem).append(`${fl('lw')}<input value='${init.circle.lw}' type='number' id="lw${max}" />`);
+            $(elem).append(`${fl('lc')}<input value='${init.circle.lc}' id="lc${max}" class='co' />`);
+            $(elem).append(`${fl('  ')}<input value='' id="du-${max}" class='dummy' disabled/>`);
+            $(elem).append("<button>+</button>");
             updateSVG();
             break;
         default:
             break;
-    }
+    }   // END of switch
 
+    
+    $(`${elem} input`).on("change", function() { updateSVG(); });
 
-    for ( let i = 6; i <= $(`#elem${pointer.max} > `).length ;  i+=2 ) {
-
-        $(`#elem${pointer.max} input:nth-child(${i})`).on("change", function() { updateSVG(); });
-
-        $(`#elem${pointer.max} input:nth-child(${i})`).on("focus", function() { 
-            $(this).css("background-color", "#ADF");
-            pointer.id = $(this).attr("id");  
-            pointer.num = $(this).siblings().eq(1).val();       // REMEMBER THIS!!! this might hurt later.
-            // code.text(pointer.num);
-        });
-
-        $(`#elem${pointer.max} input:nth-child(${i})`).on("focusout", function() { $(this).css("background-color", "#DEF"); });
-
-
-    }   // End of for
-
-    $(`#co${pointer.max}`).on("change", function() {
-        
-        let a = $(this).val();
-        if ( /[a-f|0-9|A-F]{3}/.test(a) ) {
-            
-            a = a.match(/([a-f|0-9|A-F]{3})/)[1];
-
-            $(this).val(`#${a}`);
-        }
-        
-        updateSVG();
+    $(`${elem} input`).on("focus", function() { 
+        $(this).css("background-color", color.shade);
+        pointer.c = $(this).index() + 1;
+        pointer.r = $(this).parent().index() + 1;
+        pointer.l = $(this).siblings().length;
+        code.text(`row:${pointer.r}    col:${pointer.c}     length:${pointer.l}`);
 
     });
 
+    $(`${elem} input`).on("focusout", function() { $(this).css("background-color", color.blank); });
 
+    $(`.co`).on("change", function() {
 
-    $(`#elem${pointer.max} button`).on("click", function() {
+        let a = $(this).val();
+        if ( /[a-f|0-9|A-F]{3}/.test(a) ) {
+            a = a.match(/([a-f|0-9|A-F]{3})/)[1];
+            $(this).val(`#${a}`);
+        }
+        updateSVG();
+    });     // CHANGE
+
+    $(`${elem} button`).on("click", function() {
         // When the PLUS button is clicked...
         // (1) Hide the old button.
         // (2) Create a new <div> for the next line.
 
-        // $(this).hide();
         if ( $(this).text() == '+' ) {
             $(this).text('x');
-            pointer.max++;
             Initiate();
         }
-
-    });
-
+    });     // CLICK
 
 }   // End of Populate()
 
@@ -189,8 +186,13 @@ function Populate(svgType) {
 
 
 
-function unPopulate() {
 
+
+
+
+
+
+function unPopulate() {
 
 
 }
@@ -201,55 +203,40 @@ function unPopulate() {
 function PopUp() {
     // For use later, when I make pop-up drop menu
 
+
 }
 
 
 
 
 function updateSVG() {
+    // Update codeSpace.
+    // Update artSpace.
 
-    let tempStr = '';
-    let a = [];
+    let str = '';
+    code.text(str);         // Clear the codeSpace to make room for new string.
 
-    code.text(tempStr);
-        // Clear the codeSpace to make room for new string.
+    for ( let i=1 ; i<=$(`#logicSpace > div`).length ; i++ ) {
+        
+        switch ( $(`#logicSpace div:nth-child(${i}) select:first`).val() ) {
 
-    for ( let i=0 ; i<=pointer.max ; i++ ) {
-        // Iterating through the number of lines. 
-        // The variable 'i' represents the line id.
-
-        for ( let j=6 ; j<=$(`#elem${i} > `).length ; j+=2 ) {
-            // Iterating through the number of parameters for line 'i'.
-            // If the element is a line, then there are four parameters: x1, y1, x2, and y2.
-            // So, there are four parameters, iterated through 'j' going from 3 to 6.
-
-            a.push( $(`#elem${i} input:nth-child(${j})`).val() );
-            
-        }
-
-        switch ( $(`#tp${i}`).val() ) {
             case "line":
-                tempStr += `<line x1='${a[0]}' y1='${a[1]}' x2='${a[2]}' y2='${a[3]}' style="stroke:${a[6]}; stroke-width: ${a[5]}px; stroke-linecap: ${a[7]}; " />`;    
+                str += `<line x1='${sel(i,6)}' y1='${sel(i,8)}' x2='${sel(i,10)}' y2='${sel(i,12)}' style="stroke:${sel(i,18)}; stroke-width: ${sel(i,16)}px; stroke-linecap: ${sel(i,20)}; " />`;    
                 break;
             case "rect":
-                tempStr += `<rect x='${a[0]}' y='${a[1]}' width='${a[2]}' height='${a[3]}' style="fill:${a[4]}; stroke-width:${a[5]} ;stroke:${a[6]}" rx='${a[7]}' />`;
+                str += `<rect x='${sel(i,6)}' y='${sel(i,8)}' width='${sel(i,10)}' height='${sel(i,12)}' style="fill:${sel(i,14)}; stroke-width:${sel(i,16)} ;stroke:${sel(i,18)}" rx='${sel(i,20)}' />`;
                 break;
             case "circle":
-                tempStr += `<circle cx='${a[0]}' cy='${a[1]}' r='${a[2]}' fill='${a[4]}' stroke-width='${a[5]}' stroke='${a[6]}' />`;
+                str += `<circle cx='${sel(i,6)}' cy='${sel(i,8)}' r='${sel(i,10)}' fill='${sel(i,14)}' stroke-width='${sel(i,16)}' stroke='${sel(i,18)}' />`;
                 break;
             default:
                 break;
-        }
-
-
-        a = [];
-            // Reset the temporary array EVERYTIME this for loop runs.
+        }   // END of switch
         
-    }
+    }   // END of for
 
-    
-    code.text(tempStr);
-    art.html(tempStr);
+    code.text(str);
+    art.html(str);
         // The total string is inserted into codeSpace and artSpace.
 
 };  // End of updateSVG()
@@ -259,26 +246,22 @@ function updateSVG() {
 function loadEvents() {
     // The function to contain eventlisteners for whatever wasn't covered already.
 
-    art.on("click", function(ev) { $(`#${pointer.id}`).focus(); });
-
     art.on("click", function(ev) {
         // When the mouse clicks on artSpace, enter the coordinate position into the focused <input>
-        
 
-        if ( /^x+/.test(pointer.id) ) { $(`#${pointer.id}`).val(ev.pageX - art.offset().left); }
+        code.text(`row ${pointer.r} and col ${pointer.c} and length is ${pointer.l}`);         
+        let elem = $(`#logicSpace div:nth-child(${pointer.r}) input:nth-child(${pointer.c})`);
+        elem.focus();
 
-        if ( /^y+/.test(pointer.id) ) { $(`#${pointer.id}`).val(ev.pageY - art.offset().top); }
-
-        if ( /^w+/.test(pointer.id) ) { $(`#${pointer.id}`).val(ev.pageX - art.offset().left - $(`#x-${pointer.num}`).val() ); }
-
-        if ( /^h+/.test(pointer.id) ) { $(`#${pointer.id}`).val(ev.pageY - art.offset().top - $(`#y-${pointer.num}`).val() ); }
-
-        if ( /^r-/.test(pointer.id) ) {
-            let a = Math.abs( ev.pageX - art.offset().left - $(`#x-${pointer.num}`).val() );
-            let b = Math.abs( ev.pageY - art.offset().top - $(`#y-${pointer.num}`).val() );
-            $(`#${pointer.id}`).val( Math.max(a,b) );
+        if ( elem.hasClass('x') ) { elem.val(ev.pageX - art.offset().left); }
+        if ( elem.hasClass('y') ) { elem.val(ev.pageY - art.offset().top); }
+        if ( elem.hasClass('w') ) { elem.val(ev.pageX - art.offset().left - $(`#logicSpace div:nth-child(${pointer.r}) .x`).val() ); }
+        if ( elem.hasClass('h') ) { elem.val(ev.pageY - art.offset().top - $(`#logicSpace div:nth-child(${pointer.r}) .y`).val() ); }
+        if ( elem.hasClass('r') ) {
+            let a = Math.abs( ev.pageX - art.offset().left - $(`#logicSpace div:nth-child(${pointer.r}) .x`).val() );
+            let b = Math.abs( ev.pageY - art.offset().top - $(`#logicSpace div:nth-child(${pointer.r}) .y`).val() );
+            elem.val( Math.max(a,b) );
         }
-
 
         updateSVG();
 
@@ -298,7 +281,6 @@ function loadEvents() {
 
 Initiate();
 loadEvents();
-
 
 
 // -------------------- THE END -------------------- //
